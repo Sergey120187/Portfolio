e5680048e7fd578ae8844d5740d2a9ab6d4c1333
Sergey Emelyanov <post.es2010@yandex.ru>
2022-05-10 21:41:19 +0300

    reviews

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(function(){
   var mixer = mixitup('.portfolio__gallery');
+  $('.reviews__list').slick({
+    arrows: false,
+    dots: true
+  });
 });
+
+
+
+
+Fancybox.bind("[data-fancybox]", {
+  Image: {
+    Panzoom: {
+      zoomFriction: 0.9,
+      maxScale: function () {
+        return 4;
+      },
+    },
+  },
+});
+
