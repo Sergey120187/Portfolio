315ebddf548e10def8491317b81d90b4285d5d28
Sergey Emelyanov <post.es2010@yandex.ru>
2022-05-27 16:08:38 +0300

    adaptiv

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
+  // $('[data-facnybox=".portfolio__link"]').fancybox({
+  //   "width": "1000px",
+  // })
+
   var mixer = mixitup('.portfolio__gallery');
   $('.reviews__list').slick({
     arrows: false,
