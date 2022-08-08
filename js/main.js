fbdcd9b93995a695bd7d2a34510c6d2c6e974350
Sergey Emelyanov <post.es2010@yandex.ru>
2022-08-08 11:54:46 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -6,23 +6,33 @@ $(function () {
 
   $('.header__btn, .menu__link').on('click', function () {
     $('.menu').toggleClass('menu--active'),
-    $('.btn-menu__line').toggleClass('btn-menu__line--hidden'),
-    $('body').toggleClass('fixed');
+      $('.btn-menu__line').toggleClass('btn-menu__line--hidden'),
+      $('body').toggleClass('fixed');
   });
 
   $('.menu__link').on('click', function () {
     $('body').removeClass('fixed');
   });
 
-  $(document).on('scroll', function () {
-    var position = $(window).scrollTop(),
-    about_position = $('#about').offset().top;
-    if ((position + 1) > about_position) {
+  $(window).scroll(function () {
+    var position = $(this).scrollTop();
+    if (position > 70) {
       $('.header__nav-wrapper').addClass('header__nav-wrapper--bg');
-    } else {
-      $('.header__nav-wrapper').removeClass('header__nav-wrapper--bg');
     }
+    else {
+      $('.header__nav-wrapper').removeClass('header__nav-wrapper--bg');
+    } 
   });
+
+  // $(document).on('scroll', function () {
+  //   var position = $(window).scrollTop(),
+  //   about_position = $('#about').offset().top;
+  //   if ((position + 1) > about_position) {
+  //     $('.header__nav-wrapper').addClass('header__nav-wrapper--bg');
+  //   } else {
+  //     $('.header__nav-wrapper').removeClass('header__nav-wrapper--bg');
+  //   }
+  // });
 
   $('.reviews__list').slick({
     arrows: false,
@@ -34,7 +44,7 @@ $(function () {
   $(".menu__link, .logo, .header__link").on("click", function (event) {
     event.preventDefault();
     var id = $(this).attr('href'),
-    top = $(id).offset().top;
+      top = $(id).offset().top;
     $('body,html').animate({ scrollTop: top }, 1500);
   });
 });
